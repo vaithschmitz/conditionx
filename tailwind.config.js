e2e2abccdd2a94fa0b542a68a9dc17905dcad732
Vaith Schmitz <vaithschmitz@googlemail.com>
2025-06-23 00:42:59 +0100

    vercel config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // <- include .ts and .tsx just in case
+  ],
   theme: {
     extend: {
       colors: {
